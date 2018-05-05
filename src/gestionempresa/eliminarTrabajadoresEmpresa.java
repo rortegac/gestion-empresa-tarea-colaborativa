b02d407a49f1dd5cbe0889366d5a03fc5ef60e8e
Rubén Ortega <ortegacatena.ruben@gmail.com>
2018-05-05 12:36:00 +0200

    Se inserta la funcion de eliminar en un nuevo metodo dentro de la clase eliminarTrabajadoresEmpresa

--- src/gestionempresa/eliminarTrabajadoresEmpresa.java
+++ src/gestionempresa/eliminarTrabajadoresEmpresa.java
@@ -5,10 +5,26 @@
  */
 package gestionempresa;
 
+import java.util.*;
+
 /**
  *
  * @author dmm_s
  */
 public class eliminarTrabajadoresEmpresa {
+    public static void eliminar() {
+        Iterator <Empleado> itr_emp2 = listarTrabajadoresEmpresa.listaEmpleados.iterator();
+        System.out.println("Eliminación de empleados con salario mayor de 2000 €: ");
+        System.out.println(" ");
+        while(itr_emp2.hasNext()) {
+            Empleado aeliminar = itr_emp2.next();
+
+            if(aeliminar.getSalario() > 2000) { //en caso de que ganen mÃ¡s de 2000â‚¬ eliminar dicho empleado de listaEmpleados.    
+                itr_emp2.remove();
+                System.out.println("    " + aeliminar.getNombre() + " " + aeliminar.getApellidos() + " con salario de " + aeliminar.getSalario() + " € ");
+                System.out.println("Eliminación realizada con éxito ");
+            }                                                  
+        }
+    } 
        
 }
